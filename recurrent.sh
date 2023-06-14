5dc0293082b8c35dc1b9d20487eb5571107a4da2
makai <1345744510@qq.com>
2023-06-14 13:48:18 +0800

    init

---
+++ recurrent.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export OPENAI_API_KEY="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"
+iteration=10
+outfile=response.txt
+init_prompt=init_prompt.json
+topic=Aliens
+type="science-fiction"
+
+
+options="\
+        --iter $iteration\
+        --r_file $outfile \
+        --init_prompt $init_prompt \
+        --topic $topic \
+        --type $type \
+        "
+python3 main.py $options
